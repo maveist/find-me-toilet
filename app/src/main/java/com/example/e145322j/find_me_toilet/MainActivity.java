ffb3b8581f24555614d9c01a707d8ed521487e21
maveist <maveist44@gmail.com>
2016-03-16 15:25:37 +0100

    load json data done.

--- app/src/main/java/com/example/e145322j/find_me_toilet/MainActivity.java
+++ app/src/main/java/com/example/e145322j/find_me_toilet/MainActivity.java
@@ -1,17 +1,46 @@
 package com.example.e145322j.find_me_toilet;
 
 import android.app.Activity;
+import android.app.DownloadManager;
 import android.os.Bundle;
+import android.util.JsonReader;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SpinnerAdapter;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.e145322j.find_me_toilet.data.Toilet;
+import com.google.android.gms.appdatasearch.GetRecentContextCall;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends Activity {
 
+
+    private ArrayList<Toilet> toilets;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toilets = Toilet.loadData(this);
+
+
+
     }
 
     @Override
---
+++ app/src/main/java/com/example/e145322j/find_me_toilet/data/Toilet.java
@@ -0,0 +1,171 @@
+package com.example.e145322j.find_me_toilet.data;
+
+import android.content.Context;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.e145322j.find_me_toilet.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+/**
+ * Created by E145322J on 16/03/16.
+ */
+public class Toilet {
+    public static String URL_JSON = "http://data.nantes.fr/api/publication/24440040400129_NM_NM_00170/Toilettes_publiques_nm_STBL/content";
+
+    private String id; //correspondant à name dans le json
+    private String adresse;
+    private String commune;
+    private String pole;
+    private String type;
+    private boolean automatique;
+    private boolean handiAccess;
+    private String horaires;
+    private String location;
+
+    public Toilet(String id, String adresse, String commune, String pole, String type, boolean automatique, boolean handiAccess, String horaires, String location) {
+        this.id = id;
+        this.adresse = adresse;
+        this.commune = commune;
+        this.pole = pole;
+        this.type = type;
+        this.automatique = automatique;
+        this.handiAccess = handiAccess;
+        this.horaires = horaires;
+        this.location = location;
+    }
+
+    public Toilet(JSONObject json){
+        try{
+            id = json.getJSONObject("geo").getString("name");
+            adresse = json.getString("ADRESSE");
+            commune = json.getString("COMMUNE");
+            pole = json.getString("POLE");
+            type = json.getString("TYPE");
+            automatique = json.getString("AUTOMATIQUE").equals("oui");
+            handiAccess = json.getString("ACCESSIBILITE_PMR").equals("oui");
+            location = json.getString("_l");
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public static ArrayList<Toilet> loadData(Context context){
+        ArrayList<Toilet> toilets = new ArrayList<>();
+        RequestQueue queue = Volley.newRequestQueue(context);
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (Request.Method.GET, URL_JSON, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        try {
+                            JSONArray array = response.getJSONArray("data");
+                            int len = array.length();
+                            for (int i = 0; i < len; i++) {
+                                Toilet t = new Toilet(array.getJSONObject(i));
+                                toilets.add(t);
+                            }
+                        }catch(JSONException e){
+                            e.printStackTrace();
+                        }
+                    }
+                },  new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+
+                    }
+                });
+        queue.add(jsObjRequest);
+        return toilets;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getCommune() {
+        return commune;
+    }
+
+    public void setCommune(String commune) {
+        this.commune = commune;
+    }
+
+    public String getPole() {
+        return pole;
+    }
+
+    public void setPole(String pole) {
+        this.pole = pole;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public boolean isAutomatique() {
+        return automatique;
+    }
+
+    public void setAutomatique(boolean automatique) {
+        this.automatique = automatique;
+    }
+
+    public boolean isHandiAccess() {
+        return handiAccess;
+    }
+
+    public void setHandiAccess(boolean handiAccess) {
+        this.handiAccess = handiAccess;
+    }
+
+    public String getHoraires() {
+        return horaires;
+    }
+
+    public void setHoraires(String horaires) {
+        this.horaires = horaires;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
